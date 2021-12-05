a6bac8b791ab95d76b965c720b332840e27a1e55
AR-Rahat <asfakurrahat7@gmail.com>
2021-12-05 17:49:10 +0600

    push

---
+++ script.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({
+            scrollTop: 0
+        });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Streamer", "Gamer", "Youtuber", "Bot-Developer", "Discord-Mod"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Streamer", "Gamer", "Youtuber", "Bot-Developer", "Discord-Mod"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+var btn = document.getElementById('sendmail')
+btn.onclick = function () {
+
+    console.log('This is onclick function');
+    var email = $('#email-ad').val();
+    var name = $('#name').val();
+    var msg = $('#message').val();
+    var sub = $('#subject').val();
+
+    var Body =
+        `ARRAHAT777 You got a emil from your website\n     Sender: ${name}\n      Sender Email: ${email}\nSubject:${sub}     \n\n\n      BODY:${msg}`
+
+    console.log(email, name, msg, sub);
+
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "arrahat777.gaming@gmail.com",
+        Password: "beaking12",
+        To: 'asfakurrahat7@gmail.com',
+        From: "arrahat777.gaming@gmail.com",
+        Subject: `${name} send you a mail`,
+        Body: Body
+    }).then(message => {
+        console.log(message)
+    });
+}
